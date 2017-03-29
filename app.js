b02fca591be87bc13619489b6f87c13d27c48b8a
Priscilla Parodi <pbueno@br.ibm.com>
2017-03-29 00:43:30 +0000

    test

--- app.js
+++ app.js
@@ -1,28 +1,113 @@
-/*eslint-env node*/
-
-//------------------------------------------------------------------------------
-// node.js starter application for Bluemix
-//------------------------------------------------------------------------------
-
-// This application uses express as its web server
-// for more info, see: http://expressjs.com
 var express = require('express');
-
-// cfenv provides access to your Cloud Foundry environment
-// for more info, see: https://www.npmjs.com/package/cfenv
-var cfenv = require('cfenv');
-
-// create a new express server
+var request = require('request');
+var bodyParser = require('body-parser');
+var watson = require('watson-developer-cloud');
 var app = express();
+var contexid = "";
 
-// serve the files out of ./public as our main files
-app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
-// get the app environment from Cloud Foundry
-var appEnv = cfenv.getAppEnv();
-
-// start server on the specified port and binding host
-app.listen(appEnv.port, '0.0.0.0', function() {
-  // print a message when the server starts listening
-  console.log("server starting on " + appEnv.url);
+var conversation_id = "";
+var w_conversation = watson.conversation({
+    url: 'https://gateway.watsonplatform.net/conversation/api',
+    username: process.env.CONVERSATION_USERNAME || '581c21b7-c354-4cde-87d6-e07ae8f79676',
+    password: process.env.CONVERSATION_PASSWORD || 'LEzzkiBOESmJ',
+    version: 'v1',
+    version_date: '2016-07-11'
 });
+var workspace = process.env.WORKSPACE_ID || 'workspaceId';
+
+app.get('/webhook/', function (req, res) {
+    if (req.query['hub.verify_token'] === 'EAAI2zxORuUkBAHSXZAjWmYolAbKnN4YFyTt0oWA7pDPypxu0YGCfOinn0nzePh0dQ5Dy2ZAqCHyWrcOxBO8ULQjZCf9FWZB189xYb9cYp5Y1ZCgCqPYKXpjuPFt0Cxxb4MYasQHviLhc8lRwpzbVTOb70nJM5e91NPOZBNlutPfAZDZD') {
+        res.send(req.query['hub.challenge']);
+    }
+    res.send('Erro de validação no token.');
+});
+
+app.post('/webhook/', function (req, res) {
+	var text = null;
+	
+    messaging_events = req.body.entry[0].messaging;
+	for (i = 0; i < messaging_events.length; i++) {	
+        event = req.body.entry[0].messaging[i];
+        sender = event.sender.id;
+
+        if (event.message && event.message.text) {
+			text = event.message.text;
+		}else if (event.postback && !text) {
+			text = event.postback.payload;
+		}else{
+			break;
+		}
+		
+		var params = {
+			input: text,
+			// context: {"conversation_id": conversation_id}
+			context:contexid
+		}
+
+		var payload = {
+			workspace_id: "702350b3-1725-4146-94f7-27a8ec73c7e0"
+		};
+
+		if (params) {
+			if (params.input) {
+				params.input = params.input.replace("\n","");
+				payload.input = { "text": params.input };
+			}
+			if (params.context) {
+				payload.context = params.context;
+			}
+		}
+		callWatson(payload, sender);
+    }
+    res.sendStatus(200);
+});
+
+function callWatson(payload, sender) {
+	w_conversation.message(payload, function (err, convResults) {
+		 console.log(convResults);
+		contexid = convResults.context;
+		
+        if (err) {
+            return responseToRequest.send("Erro.");
+        }
+		
+		if(convResults.context != null)
+    	   conversation_id = convResults.context.conversation_id;
+        if(convResults != null && convResults.output != null){
+			var i = 0;
+			while(i < convResults.output.text.length){
+				sendMessage(sender, convResults.output.text[i++]);
+			}
+		}
+            
+    });
+}
+
+function sendMessage(sender, text_) {
+	text_ = text_.substring(0, 319);
+	messageData = {	text: text_ };
+
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: { access_token: token },
+        method: 'POST',
+        json: {
+            recipient: { id: sender },
+            message: messageData,
+        }
+    }, function (error, response, body) {
+        if (error) {
+            console.log('Error sending message: ', error);
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error);
+        }
+    });
+};
+
+var token = "EAAI2zxORuUkBAHSXZAjWmYolAbKnN4YFyTt0oWA7pDPypxu0YGCfOinn0nzePh0dQ5Dy2ZAqCHyWrcOxBO8ULQjZCf9FWZB189xYb9cYp5Y1ZCgCqPYKXpjuPFt0Cxxb4MYasQHviLhc8lRwpzbVTOb70nJM5e91NPOZBNlutPfAZDZD";
+var host = (process.env.VCAP_APP_HOST || 'localhost');
+var port = (process.env.VCAP_APP_PORT || 3000);
+app.listen(port, host);
